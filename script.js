2b503643dc98d0a6339063b6424226e2dd5fa230
AvilaAi <avila911008@gmail.com>
2019-11-27 14:59:14 +0100

    modif couleur

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
-
-
-let buttons =document.getElementsByClassName("trash")
-let p = document.createElement("div");
-p.className  = "contact"
+let buttons = document.getElementsByClassName('trash');
+let p = document.createElement('div');
+p.className = 'contact';
 document.body.appendChild(p);
-let newText = document.createTextNode("Jogging avec Jules samedi 10h00");
-p.appendChild( newText );
-p.addEventListener("mouseover",function(){p.style.color="blue"});
+let newText = document.createTextNode('Jogging avec Jules samedi 10h00');
+p.appendChild(newText);
+p.addEventListener('mouseover', function() {
+	p.style.color = 'blue';
+});
 
-let contacts = document.getElementsByClassName("contact")
+let contacts = document.getElementsByClassName('contact');
 
+document.getElementById('nombre').textContent = contacts.length;
 
-// var msg = contacts.length;
-// document.getElementById("nombre").textContent= msg;
-document.getElementById("nombre").textContent=contacts.length;
-
-for (var i=0;i<contacts.length-1;i++){
-  buttons[i].addEventListener(
-  "click",function(){
-  // document.body.removeChild(this.parentNode);
-  console.log("click détecté");
-  this.parentElement.remove();
-  // var msg = contacts.length;
-  document.getElementById("nombre").textContent= contacts.length;
-
-})
+for (var i = 0; i < contacts.length - 1; i++) {
+	buttons[i].addEventListener('click', function() {
+		console.log('click détecté');
+		this.parentElement.remove();
+		document.getElementById('nombre').textContent = contacts.length;
+	});
 }
 
+let first = document.body.firstChild;
+document.body.insertBefore(p, first);
 
-let first= document.body.firstChild;
-document.body.insertBefore(p,first);
-
-for (var u=0; u<contacts.length;u++){contacts[u].addEventListener("mouseover",function(){this.style.color="red"})};
-// contacts.addEventListener("mouseup",function(){contacts.style.color="red"});
+for (var u = 0; u < contacts.length; u++) {
+	contacts[u].addEventListener('mouseover', function() {
+		this.style.color = 'black';
+	});
+}
